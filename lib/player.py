7bf00e3d07319b5a69dbf9563f61c127590c4c50
pannal <1359593+pannal@users.noreply.github.com>
2026-02-06 11:22:28 +0100

    Player: Fix embedded subtitles being set wrong (partially reverts previous commit)

--- lib/player.py
+++ lib/player.py
@@ -1333,7 +1333,7 @@ class SeekPlayerHandler(BasePlayerHandler):
                     except:
                         currentIdx = -1
 
-                    if self._lastSetEmbeddedSubIdx == targetIdx or currentIdx == targetIdx:
+                    if currentIdx == targetIdx:
                         util.DEBUG_LOG('Embedded subtitle stream already set to: {0} (kodi: {1}), skipping',
                                        targetIdx, currentIdx)
                         self._lastSetEmbeddedSubIdx = targetIdx
