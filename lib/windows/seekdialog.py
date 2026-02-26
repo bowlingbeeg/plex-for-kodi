1469ac58b4f3b58aacb1709bc368a2c719d1e758
pannal <1359593+pannal@users.noreply.github.com>
2026-02-26 16:50:02 +0100

    SeekDialog: Don't react to SELECT/ENTER events on skip marker buttons during seek

--- lib/windows/seekdialog.py
+++ lib/windows/seekdialog.py
@@ -649,6 +649,9 @@ class SeekDialog(kodigui.BaseDialog, windowutils.GoHomeMixin, PlexSubtitleDownlo
                 passThroughMain = False
                 if controlID == self.SKIP_MARKER_BUTTON_ID:
                     if action == xbmcgui.ACTION_SELECT_ITEM:
+                        if self._seeking:
+                            return
+
                         markerDef = self._currentMarker
                         if markerDef["marker"]:
                             marker = markerDef["marker"]
