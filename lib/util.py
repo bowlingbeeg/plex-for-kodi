0c589685bc40c7c05d89b84d7c19b592a8f3d7c3
pannal <1359593+pannal@users.noreply.github.com>
2026-03-16 16:03:27 +0100

    bump theme version

--- lib/util.py
+++ lib/util.py
@@ -48,7 +48,7 @@ PROFILE = translatePath(ADDON.getAddonInfo('profile'))
 
 
 DEF_THEME = "modern-colored"
-THEME_VERSION = 86
+THEME_VERSION = 87
 
 UI_INTERVAL = 1 / float(addonSettings.uiWaitRate)
 
